5b50be757077a3b61d564f16a8a856c2764268a6
AmartyaDev007 <AmartyaDev007@users.noreply.github.com>
2025-07-30 00:07:27 +0530

    add 3 files with some basic functionality

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # basic-library
+
+Simple library excercise from odin-project.
+
+- Instructions: https://www.theodinproject.com/lessons/node-path-javascript-library
---
+++ code.js
@@ -0,0 +1,14 @@
+const myLibrary = [];
+
+function Book(title, author, pages) { 
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+
+
+}
+
+function addBookToLibrary(title, author, pages) { 
+    myLibrary.push(new Book(title, author, pages));
+}
+
